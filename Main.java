c160d62b0d04eb608d12ef760f70235ecf570a98
nvermill12 <141042989+nvermill12@users.noreply.github.com>
2024-05-27 10:11:24 -0500

    Heck yeah, I'm back! Natalie Rosemarie

---
+++ Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args){
+
+        System.out.println("Natalie Rosemarie");
+
+        String name = "Natalie Rosemarie";
+
+        for (int i = 0; i < name.length(); i++) {
+            System.out.print(name.charAt(i));
+        }
+
+    }
+}
