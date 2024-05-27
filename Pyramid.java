1059996d4b026ceae562693a9da156525531e8b6
nvermill12 <141042989+nvermill12@users.noreply.github.com>
2024-05-27 18:16:13 -0500

    Pyramid

--- Pyramid.java
+++ Pyramid.java
@@ -1,13 +1,21 @@
 public class Pyramid {
     public static void main(String[] args) {
 
+        func(4);
+
     }
 
     public static void func(int size){
 
         for (int i = 0; i < size; i++) {
+            String asterisk = "";
 
+            for (int j = 0; j < i + 1; j++) {
+                asterisk += "*";
+            }
+            System.out.println(asterisk);
         }
+
 
     }
 }
