5206e62878fa7ab31de461178cfd52489b30a418
nvermill12 <141042989+nvermill12@users.noreply.github.com>
2024-05-27 18:12:46 -0500

    Heck yeah, I'm back! Natalie Rosemarie

---
+++ Pyramid.java
@@ -0,0 +1,13 @@
+public class Pyramid {
+    public static void main(String[] args) {
+
+    }
+
+    public static void func(int size){
+
+        for (int i = 0; i < size; i++) {
+
+        }
+
+    }
+}
