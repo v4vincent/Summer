273edf69da054a6ddc4e34d0f17540410b89a804
nvermill12 <141042989+nvermill12@users.noreply.github.com>
2024-06-03 11:36:41 -0500

    js password strength

--- script.js
+++ script.js
@@ -1,8 +1,30 @@
 
 function isStrongPassword(password) {
     if (password.length > 7) {
-        console.log("True");
-    } else console.log("False")
+        if (password !== "password"){
+            if (isUpperPassword(password)){
+                console.log("true 3")
+            } else console.log("false - No uppercase characters")
+        } else console.log("false - Contains \"password\"")
+    } else console.log("false - Too short")
 }
 
-isStrongPassword("test")
+
+
+isStrongPassword("passwordQwerty");
+
+
+
+function isUpperPassword(str) {
+    for (let i = 0; i < str.length; i++) {
+
+        if (!Number.isNaN(str.charAt(i))){
+            return false;
+        }
+        if (str.charAt(i) === str.charAt(i).toUpperCase()) {
+            return true;
+        }
+    }
+    return false;
+}
+
